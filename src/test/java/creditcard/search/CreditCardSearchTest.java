1e24ed15c706e4e21f474a800814e771b7582379
Stefan Heimberg <kontakt@stefanheimberg.ch>
2015-08-12 00:05:19 +0200

    implementation posted on stackoverflow

--- src/test/java/creditcard/search/CreditCardSearchTest.java
+++ src/test/java/creditcard/search/CreditCardSearchTest.java
@@ -1,45 +1,46 @@
-package creditcard.search;
+    package creditcard.search;
 
-import java.io.File;
-import java.util.Set;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import org.junit.Before;
-import org.junit.Test;
+    import java.io.File;
+    import java.io.IOException;
+    import java.util.Set;
+    import static org.junit.Assert.assertEquals;
+    import static org.junit.Assert.assertNotNull;
+    import static org.junit.Assert.assertTrue;
+    import org.junit.Before;
+    import org.junit.Test;
 
-public class CreditCardSearchTest {
+    public class CreditCardSearchTest {
 
-    private CreditCardSearch creditCardSearch;
+        private CreditCardSearch creditCardSearch;
 
-    @Before
-    public void setUp() {
-        creditCardSearch = new CreditCardSearch();
-        creditCardSearch.addCreditCardsFile(new File("creditCards1.txt"));
-        creditCardSearch.addCreditCardsFile(new File("creditCards2.txt"));
+        @Before
+        public void setUp() {
+            creditCardSearch = new CreditCardSearch();
+            creditCardSearch.addCreditCardsFile(new File("creditCards1.txt"));
+            creditCardSearch.addCreditCardsFile(new File("creditCards2.txt"));
+        }
+
+        @Test
+        public void assert_creditcards_found_in_2_files() throws IOException {
+            final Set<String> creditcards = creditCardSearch.searchForMatchesInAllFiles();
+
+            assertNotNull(creditcards);
+            assertEquals(3, creditcards.size());
+            assertTrue("5154933849210203", creditcards.contains("5154933849210203"));
+            assertTrue("5272835420269481", creditcards.contains("5272835420269481"));
+            assertTrue("5572941594858518", creditcards.contains("5572941594858518"));
+        }
+
+        @Test
+        public void assert_creditcards_found_in_3_files() throws IOException {
+            creditCardSearch.addCreditCardsFile(new File("creditCards3.txt"));
+
+            final Set<String> creditcards = creditCardSearch.searchForMatchesInAllFiles();
+
+            assertNotNull(creditcards);
+            assertEquals(2, creditcards.size());
+            assertTrue("5154933849210203", creditcards.contains("5154933849210203"));
+            assertTrue("5272835420269481", creditcards.contains("5272835420269481"));
+        }
+
     }
-
-    @Test
-    public void assert_creditcards_found_in_2_files() {
-        final Set<String> creditcards = creditCardSearch.searchForMatchesInAllFiles();
-        
-        assertNotNull(creditcards);
-        assertEquals(3, creditcards.size());
-        assertTrue("5154933849210203", creditcards.contains("5154933849210203"));
-        assertTrue("5272835420269481", creditcards.contains("5272835420269481"));
-        assertTrue("5572941594858518", creditcards.contains("5572941594858518"));
-    }
-    
-    @Test
-    public void assert_creditcards_found_in_3_files() {
-        creditCardSearch.addCreditCardsFile(new File("creditCards3.txt"));
-        
-        final Set<String> creditcards = creditCardSearch.searchForMatchesInAllFiles();
-        
-        assertNotNull(creditcards);
-        assertEquals(3, creditcards.size());
-        assertTrue("5154933849210203", creditcards.contains("5154933849210203"));
-        assertTrue("5272835420269481", creditcards.contains("5272835420269481"));
-    }
-
-}
