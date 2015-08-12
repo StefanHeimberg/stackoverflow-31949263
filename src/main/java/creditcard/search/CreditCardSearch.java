8ac475b12a051cd69b4de8b49c9802df1ce4205f
Stefan Heimberg <kontakt@stefanheimberg.ch>
2015-08-12 23:19:06 +0200

    corrected indent

--- src/main/java/creditcard/search/CreditCardSearch.java
+++ src/main/java/creditcard/search/CreditCardSearch.java
@@ -1,53 +1,53 @@
-    package creditcard.search;
+package creditcard.search;
 
-    import java.io.File;
-    import java.io.IOException;
-    import java.nio.file.Files;
-    import java.nio.file.Path;
-    import java.util.HashMap;
-    import java.util.HashSet;
-    import java.util.Iterator;
-    import java.util.List;
-    import java.util.Map;
-    import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-    public class CreditCardSearch {
+public class CreditCardSearch {
 
-        private final Set<File> files = new HashSet();
+    private final Set<File> files = new HashSet();
 
-        public void addCreditCardsFile(final File file) {
-            files.add(file);
-        }
-
-        public Set<String> searchForMatchesInAllFiles() throws IOException {
-            if(files.size() < 2) {
-                throw new IllegalArgumentException("a minimum of 2 creditcard files required");
-            }
-
-            final Map<Path, List<String>> linesPerPath = new HashMap<>();
-            for(final File file : files) {
-                final Path path = file.toPath();
-                linesPerPath.put(path, Files.readAllLines(path));
-            }
-
-            final Set<String> creditcards = new HashSet();
-
-            final Iterator<Path> it = linesPerPath.keySet().iterator();
-
-            List<String> linesA = null;
-
-            while(it.hasNext()) {
-                if(null == linesA) {
-                    linesA = linesPerPath.get(it.next());
-                }
-                List<String> linesB = linesPerPath.get(it.next());
-
-                if(linesA.retainAll(linesB)) {
-                    creditcards.addAll(linesA);
-                }
-            }
-
-            return creditcards;
-        }
-
+    public void addCreditCardsFile(final File file) {
+        files.add(file);
     }
+
+    public Set<String> searchForMatchesInAllFiles() throws IOException {
+        if (files.size() < 2) {
+            throw new IllegalArgumentException("a minimum of 2 creditcard files required");
+        }
+
+        final Map<Path, List<String>> linesPerPath = new HashMap<>();
+        for (final File file : files) {
+            final Path path = file.toPath();
+            linesPerPath.put(path, Files.readAllLines(path));
+        }
+
+        final Set<String> creditcards = new HashSet();
+
+        final Iterator<Path> it = linesPerPath.keySet().iterator();
+
+        List<String> linesA = null;
+
+        while (it.hasNext()) {
+            if (null == linesA) {
+                linesA = linesPerPath.get(it.next());
+            }
+            List<String> linesB = linesPerPath.get(it.next());
+
+            if (linesA.retainAll(linesB)) {
+                creditcards.addAll(linesA);
+            }
+        }
+
+        return creditcards;
+    }
+
+}
